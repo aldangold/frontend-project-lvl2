06925e97da665be54dd9615f3bd05efd59a492d6
aldangold <meloshnikoff@gmail.com>
2021-07-17 14:41:05 +0300

    step2: added args(filepath1-2) in commandline tools

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,7 +2,9 @@
 import program from 'commander';
 
 program
-  .version('0.1.0')
-  .description('Compares two configuration files and shows a difference.');
+  .version('0.2.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
 
-program.parse();
+program.parse(process.argv);
